9884ae383577e4e94f77e2fcf870c6219edef227
Carranya <k.gong82@gmail.com>
2022-09-18 20:57:36 +0200

    done some format

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Proposal 2</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <div id="headline">Head</div>
+    <div id="menu">
+        <?php
+            include "pages/menu.php";
+        ?>
+    </div>
+    <div id="main">Main</div>
+    <div id="footline">Foot</div>
 </body>
 </html>
+
