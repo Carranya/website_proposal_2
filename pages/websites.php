eebf6707aa88ede84d5b37ba675318aec0cd15dd
Carranya <k.gong82@gmail.com>
2022-10-16 07:41:04 +0200

    add website proposal 3

--- pages/websites.php
+++ pages/websites.php
@@ -13,6 +13,7 @@ Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und
             <a href="https://github.com/Carranya/homepage_v1" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></img></a>
             <a href="https://github.com/Carranya/homepage_v2" target="_blank"><img src="img/websites/homepage_v2.jpg" class="websitesPic"></img></a>
             <a href="https://github.com/Carranya/website_proposal_1" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></img></a>
+            <a href="https://github.com/Carranya/website_proposal_3" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></img></a>
         </p>
     </div>
     
