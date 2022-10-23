2e40f434b6650e661ef3aff4d6ce7994d85a25d3
Carranya <k.gong82@gmail.com>
2022-10-23 10:14:38 +0200

    added sample pages

--- newpage/index.php
+++ newpage/index.php
@@ -5,8 +5,21 @@
     <meta http-euqiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Proposal 2</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/menu.js"></script>
 </head>
 <body>
+    <div id="main">
+        <div class="classPage">r</div>
+        <div id="home" class="classPage">Home</div>
+        <div class="classPage">r</div>
+        <div id="websites" class="classPage">Websites</div>
+        <div class="classPage">r</div>
+        <div id="about" class="classPage">About</div>
+        <div class="classPage">r</div>
+        <div id="contact" class="classPage">Contact</div>
+        <div class="classPage">r</div>
+    </div>
 </body>
 </html>
 
