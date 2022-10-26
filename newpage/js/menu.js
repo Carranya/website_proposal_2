aa8bd84496b9a5e2c468d8d349ade390f46fb196
Carranya <k.gong82@gmail.com>
2022-10-27 00:02:14 +0200

    added function

--- newpage/js/menu.js
+++ newpage/js/menu.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-    // let home = document.getElementById("main"); home.style.transform = "translate(-33.4%, 0)";
+    let home = document.getElementById("main"); home.style.transform = "translate(-33.4%, 0)";
     // let websites = document.getElementById("main"); websites.style.transform = "translate(0, -33.4%)";
     // let about = document.getElementById("main"); about.style.transform = "translate(-66.8%, -33.4%)"; 
-    let contact = document.getElementById("main"); contact.style.transform = "translate(-33.4%, -66.8%)";
+    // let contact = document.getElementById("main"); contact.style.transform = "translate(-33.4%, -66.8%)";
 
     
 }
