55d98bc6ce4e222e9c6eabff5d898c27b16deb64
Carranya <k.gong82@gmail.com>
2022-10-30 00:17:42 +0200

    added page menu

---
+++ newpage/pages/menu.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><button id="idHome" value="home">Home</button></li>
+    <li><button id="idWebsites"value="websites">Websites</button></li>
+    <li><button id="idContact"value="contact">Contact</button></li>
+    <li><button id="idAbout"value="about">About</button></li>
+</ul>
+
