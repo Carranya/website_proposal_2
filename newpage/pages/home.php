44ccfcd9b7cf0afdc884afd65ddb544bb70c7804
Carranya <k.gong82@gmail.com>
2022-11-01 07:03:05 +0100

    mod home page

--- newpage/pages/home.php
+++ newpage/pages/home.php
@@ -1,8 +1,6 @@
-<div id="home">
-    <h1 class="classTitle">Herzlich Willkommen</h1>
-    <p>
-    Diese Webseite dient als Vorschlag für ein Projekt.<br>
-    Es ist schlicht aufgebaut, aber die Menü-Buttons sind mit Javascript gesteuert.<br>
-    Als Beispiel zum verschieden Arten von Menü-Buttons zu steuern, habe ich bei "Home" und "Websites" mit klassischen<br>
-    Mausklick zur Bestätigung eingebaut, aber die Menü-Button "Contact" und "About" kann man mit Hover aktivieren.</p>
-</div>
+<h1>Herzlich Willkommen</h1>
+<p>
+Diese Webseite dient als Vorschlag für ein Projekt.<br>
+Es ist schlicht aufgebaut, aber die Menü-Buttons sind mit Javascript gesteuert.<br>
+Als Beispiel zum verschieden Arten von Menü-Buttons zu steuern, habe ich bei "Home" und "Websites" mit klassischen<br>
+Mausklick zur Bestätigung eingebaut, aber die Menü-Button "Contact" und "About" kann man mit Hover aktivieren.</p>
