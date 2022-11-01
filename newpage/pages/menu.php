bac87070dcdd63bb9f024d2b4e614c79e092e131
Carranya <k.gong82@gmail.com>
2022-11-01 07:03:25 +0100

    delete menu page

--- newpage/pages/menu.php
+++
@@ -1,7 +0,0 @@
-<ul>
-    <li><button id="idHome" value="home">Home</button></li>
-    <li><button id="idWebsites"value="websites">Websites</button></li>
-    <li><button id="idContact"value="contact">Contact</button></li>
-    <li><button id="idAbout"value="about">About</button></li>
-</ul>
-
