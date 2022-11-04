b3169d0b254b41a8f5ca91d8b9054d577885888d
Carranya <k.gong82@gmail.com>
2022-11-04 07:05:30 +0100

    created innerPage

--- newpage/pages/home.php
+++ newpage/pages/home.php
@@ -1,8 +1,10 @@
-<div class="contactPage">
-<h1>Herzlich Willkommen</h1>
-<p>
-Diese Webseite dient als Vorschlag für ein Projekt.<br>
-Es ist schlicht aufgebaut, aber die Menü-Buttons sind mit Javascript gesteuert.<br>
-Als Beispiel zum verschieden Arten von Menü-Buttons zu steuern, habe ich bei "Home" und "Websites" mit klassischen<br>
-Mausklick zur Bestätigung eingebaut, aber die Menü-Button "Contact" und "About" kann man mit Hover aktivieren.</p>
+<div class="centerPage">
+    <div class="innerPage">
+        <h1>Herzlich Willkommen</h1>
+        <p>
+        Diese Webseite dient als Vorschlag für ein Projekt.<br>
+        Es ist schlicht aufgebaut, aber die Menü-Buttons sind mit Javascript gesteuert.<br>
+        Als Beispiel zum verschieden Arten von Menü-Buttons zu steuern, habe ich bei "Home" und "Websites" mit klassischen<br>
+        Mausklick zur Bestätigung eingebaut, aber die Menü-Button "Contact" und "About" kann man mit Hover aktivieren.</p>
+    </div>
 </div>
