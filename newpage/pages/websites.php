c7f86d51a9e2307ce9e2ff958229ddd81c9341a2
Carranya <k.gong82@gmail.com>
2022-11-04 07:05:33 +0100

    created innerPage

--- newpage/pages/websites.php
+++ newpage/pages/websites.php
@@ -1,14 +1,18 @@
-<h2>Webseiten Vorschläge</h2>
-<p>
-Am Liebsten programmiere ich Webseiten.<br>
-Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
-<br>
-<p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
-<div class="websitesContent">
-    <p>
-        <a href="https://github.com/Carranya/homepage_v1" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></img></a>
-        <a href="https://github.com/Carranya/homepage_v2" target="_blank"><img src="img/websites/homepage_v2.jpg" class="websitesPic"></img></a>
-        <a href="https://github.com/Carranya/website_proposal_1" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></img></a>
-        <a href="https://github.com/Carranya/website_proposal_3" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></img></a>
-    </p>
+<div class="centerPage">
+    <div class="innerPage">
+        <h2>Webseiten Vorschläge</h2>
+        <p>
+        Am Liebsten programmiere ich Webseiten.<br>
+        Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
+        <br>
+        <p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
+        <div class="websitesContent">
+            <p>
+                <a href="https://github.com/Carranya/homepage_v1" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></img></a>
+                <a href="https://github.com/Carranya/homepage_v2" target="_blank"><img src="img/websites/homepage_v2.jpg" class="websitesPic"></img></a>
+                <a href="https://github.com/Carranya/website_proposal_1" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></img></a>
+                <a href="https://github.com/Carranya/website_proposal_3" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></img></a>
+            </p>
+        </div>
+    </div>
 </div>
