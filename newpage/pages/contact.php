cbcb4727f7327c70302bf5d6cac10fd8ad016cec
Carranya <k.gong82@gmail.com>
2022-11-08 09:45:32 +0100

    added big icons

--- newpage/pages/contact.php
+++ newpage/pages/contact.php
@@ -8,10 +8,10 @@
         Schützenmattstrasse 7<br>
         8302 Kloten<br></p>
         <p><b>Kontaktinformationen:</b><br>
-        <img src="img/icons/phone.png" width="20" height="20"></img> Mobil: 078 / 823 67 56<br>
-        <img src="img/icons/mail.png" width="20" height="20"></img> E-Mail: <u><a href="mailto:m.giang82@gmail.com">m.giang82@gmail.com</a></u><br>
-        <img src="img/icons/linkedin.png" width="20" height="20"></img> LinkedIn: <u><a href="https://www.linkedin.com/in/karin-giang-0a6072247" target="_blank">https://www.linkedin.com/in/karin-giang-0a6072247</a></u><br>
-        <img src="img/icons/github.png" width="20" height="20"></img> GitHub: <u><a href="https://github.com/Carranya" target="_blank">https://github.com/Carranya</a></u></p>
+        <img src="img/icons/phone.png" width="20" height="20"> Mobil: 078 / 823 67 56<br>
+        <img src="img/icons/mail.png" width="20" height="20"> E-Mail: <u><a href="mailto:m.giang82@gmail.com">m.giang82@gmail.com</a></u><br><br>
+        <a href="https://www.linkedin.com/in/karin-giang-0a6072247" target="_blank"><img src="img/icons/linkedin.png" class="contactBigIcons"></a>
+        <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" class="contactBigIcons"></a>
         <p><b>Sonstige Informationen:</b><br>
         Geburtstagsdatum:. 15. Februar 1982<br>
         Zivilstand: verheiratet<br>
