62fc3f35b33d257365825010426e4c751dd6dad5
Carranya <k.gong82@gmail.com>
2022-11-08 10:24:17 +0100

    added Github Logo

--- newpage/pages/websites.php
+++ newpage/pages/websites.php
@@ -3,9 +3,8 @@
         <h2>Webseiten Vorschläge</h2>
         <p>
         Am Liebsten programmiere ich Webseiten.<br>
-        Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
-        <br>
-        <p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
+        Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.<br>
+        Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
         <div class="websitesContent">
             <p>
                 <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></img></a>
