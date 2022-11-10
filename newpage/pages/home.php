97badae4cc01dbef60b2fb0e79ffeb9c9d5901a8
Carranya <k.gong82@gmail.com>
2022-11-10 10:07:44 +0100

    new home text

--- newpage/pages/home.php
+++ newpage/pages/home.php
@@ -4,8 +4,11 @@
         <h2>Herzlich Willkommen</h2>
         <p>
         Diese Webseite dient als Vorschlag für ein Projekt.<br>
-        Es ist schlicht aufgebaut, aber die Menü-Buttons sind mit Javascript gesteuert.<br>
-        Als Beispiel zum verschieden Arten von Menü-Buttons zu steuern, habe ich bei "Home" und "Websites" mit klassischen<br>
-        Mausklick zur Bestätigung eingebaut, aber die Menü-Button "Contact" und "About" kann man mit Hover aktivieren.</p>
+        Es ist schlicht aufgebaut, aber die Menü-Buttons sowie die Navigationsbar sind mit Javascript gesteuert.<br><br>
+
+        Ursprünglich wollte die diese Seite mit Slideeffekt ausstatten.<br>
+        Stattdessen habe ich entschieden, etwas mehr Fancy einzubauen.
+        </p>
+        <p>Meine offizielle Webseite: <span id="officalHomepage"><a href="http://www.karingiang.ch" target="_blank">Hier klicken</span></a></p>
     </div>
 </div>
